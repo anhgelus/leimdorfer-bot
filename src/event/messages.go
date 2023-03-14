5d06db20ef16ceff76dc0791d07b187208058845
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2023-03-14 21:41:53 +0100

    feat(timeout) timeout user after 5 warnings

--- src/event/messages.go
+++ src/event/messages.go
@@ -2,6 +2,7 @@ package event
 
 import (
 	"github.com/bwmarrin/discordgo"
+	"leimdorfer-bot/src/users"
 	"leimdorfer-bot/src/utils"
 	"math/rand"
 	"strings"
@@ -24,10 +25,32 @@ func HandleMessages(s *discordgo.Session, m *discordgo.MessageCreate) {
 }
 
 func handleDuCoup(s *discordgo.Session, m *discordgo.MessageCreate) {
+	guildID := m.GuildID
 	rand.NewSource(time.Now().UnixNano())
 	v := rand.Intn(len(Replacers))
 	msg := utils.NewCaseInsensitiveReplacer("du coup", Replacers[v]).Replace(m.Content)
-	ref := discordgo.MessageReference{MessageID: m.ID, ChannelID: m.ChannelID, GuildID: m.GuildID}
+	ref := discordgo.MessageReference{MessageID: m.ID, ChannelID: m.ChannelID, GuildID: guildID}
 	_, err := s.ChannelMessageSendReply(m.ChannelID, msg, &ref)
+	utils.HandlePanic(err)
+	// Get the user and update the warning counter
+	user := users.GetUser(m.Author, m.GuildID)
+	user.Warning += 1
+	user.Save()
+
+	// Check if we must time out the user
+	if user.Warning != 5 {
+		return
+	}
+	// Reset the warning count
+	user.Warning = 0
+	user.Save()
+	// Send the informative message
+	_, err = s.ChannelMessageSendReply(m.ChannelID, "**Get Leimdorfered!**\nTu viens de te faire timeout pour 60 secondes !", &ref)
+	utils.HandlePanic(err)
+	// Calc the time
+	d := 60 * time.Second
+	t := time.Now().Add(d)
+	// Time out the user
+	err = s.GuildMemberTimeout(guildID, user.ID, &t)
 	utils.HandlePanic(err)
 }
